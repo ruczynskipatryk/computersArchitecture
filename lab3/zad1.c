326d948b9607a855e4f470718b3c48e505337248
Patryk Ruczynski <ptrkk17@gmail.com>
2023-11-05 19:55:05 +0100

    Update

--- lab3/zad1.c
+++ lab3/zad1.c
@@ -5,19 +5,27 @@
 // Funkcja konwertująca liczbę dziesiętną na format IEEE 754
 uint32_t floatToIEEE754(float num) {
     uint32_t result = 0;
-    
-    // Przekształć float na 32-bitową reprezentację IEEE 754
     memcpy(&result, &num, sizeof(float));
-    
     return result;
 }
 
 int main() {
-    float decimalValue = 4.368; // Tutaj podaj swoją liczbę dziesiętną
+    int I1 = 4;  // Przykładowe wartości dla Imie1 i Nazwisko1
+    int N1 = 3;
+    int I2 = 6;  // Przykładowe wartości dla Imie2 i Nazwisko2
+    int N2 = 8;
     
-    uint32_t ieee754Value = floatToIEEE754(decimalValue);
+    // Obliczenie liczba_dec: I1 + N1 + I2 + N2
+    float liczba_dec = (float)(I1 + N1 + I2 + N2);
     
-    printf("Liczba dziesiętna: %f\n", decimalValue);
+    // Konwersja liczba_dec na format IEEE 754
+    uint32_t ieee754Value = floatToIEEE754(liczba_dec);
+    
+    printf("I1: %d\n", I1);
+    printf("N1: %d\n", N1);
+    printf("I2: %d\n", I2);
+    printf("N2: %d\n", N2);
+    printf("Liczba dziesiętna: %f\n", liczba_dec);
     printf("Reprezentacja IEEE 754: 0x%08X\n", ieee754Value);
     
     return 0;
